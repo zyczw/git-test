edadbf8a4954f9d57b14fec90e537c43fe68271f
zhou <zhou@qq.com>
2021-08-28 09:37:08 +0800

    push commit

--- src/main/java/com/atguigu/git/GetTest.java
+++ src/main/java/com/atguigu/git/GetTest.java
@@ -6,5 +6,6 @@ public class GetTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git3");
+        System.out.println("push github");
     }
 }
